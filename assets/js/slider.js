3fa444b580a7be8bdbf763111e0a3cd809b9eb7e
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2023-01-20 20:32:56 -0300

    create section about us
    create section about us

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,7 @@
 let timer = 2000; // 2 segundos
 let currentImage = 0; // Imagem atual
 const images = document.querySelectorAll("#slider img");
-const max = images.length;
+const max = images.length; // quantidade total de imagens
 
 function nextImage() {
 
