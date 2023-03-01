d1dd76b8608832d5b412b8216b3b6e44198c948b
BTMiranda <68792743+BTMiranda@users.noreply.github.com>
2023-03-01 18:40:01 -0300

    Site parte Final
    - Parte de Parcerias
    + Inicio da responsividade
    + alteração do body no css

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,15 +9,46 @@ function nextImage() {
   currentImage++;
   if (currentImage >= max) {
     currentImage = 0;
-  }
+  } 
   images[currentImage].classList.add("selected");
-
+  
 }
 
 function start() {
   setInterval(nextImage, timer);
 }
 
-setInterval(pularimagem, 2000);
+// Slide de Depoimentos (inicio)
 
-window.addEventListener("load", start);
+var slideIndex = 1;
+showSlides(slideIndex);
+
+setInterval(function() {
+  plusSlides(1);
+}, 10000);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// Slide de Depoimentos (Fim)
