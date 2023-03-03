8f50509586f3059adc2da7bfcaf4fb6aa84c49ec
Carlos Eduardo <68128063+Carlosmax1@users.noreply.github.com>
2023-03-03 10:49:00 -0300

    site 2.0
    - Site Antigo
    + Site refatorado utilizando a metodologia do mobile first

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,6 @@
 let timer = 3000; //  segundos
 let currentImage = 0; // Imagem atual
-const images = document.querySelectorAll("#slider img");
+const images = document.querySelectorAll(".slider img");
 const max = images.length; // quantidade total de imagens
 
 function nextImage() {
@@ -54,3 +54,5 @@ function showSlides(n) {
 }
 
 // Slide de Depoimentos (Fim)
+
+start();
