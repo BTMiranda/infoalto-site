9b2c1c0e99aea9e313c2dd425312269cb361312c
Carlosmax1 <carlosmaximoinsta@gmail.com>
2023-03-31 13:42:17 -0300

    correção da função que calcula a data

--- assets/js/ps.js
+++ assets/js/ps.js
@@ -1,19 +1,23 @@
-function getTimeDifferencePercentage(start, end) {
-  const timeDiff = Math.abs(end.getTime() - start.getTime());
+function getDateDiffPercent() {
+  // set the dates
+  const date1 = new Date("2023-03-27");
+  const date2 = new Date("2023-04-02");
 
-  const percentageDiff =
-    (timeDiff / Math.max(start.getTime(), end.getTime())) * 100;
+  // calculate the difference in milliseconds
+  const diffMs = Math.abs(date2 - date1);
 
-  return `${percentageDiff}%`;
+  // calculate the difference as a percentage of the total number of milliseconds in a week (604800000)
+  const diffPercent = (diffMs / 604800000) * 100;
+
+  // return the result as a string with two decimal places
+  return diffPercent.toFixed(2) + "%";
 }
 
 const overlay = document.getElementById("overlay");
-const start = new Date("2023-03-27");
-const end = new Date("2023-04-02");
 
 window.onload = function () {
   overlay.style.width = "0%";
   setTimeout(() => {
-    overlay.style.width = getTimeDifferencePercentage(start, end);
+    overlay.style.width = getDateDiffPercent();
   }, 1000);
 };
